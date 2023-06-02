88958f5b282501354593307d07dacd4eda67f0f3
codebrain.arthur.schutz <codebrain.arthur.schutz@kiwigrid.com>
2023-06-02 19:32:05 -0300

    GAT-3 criar entidade 'Produto'

--- src/main/java/com/acert/delivery/entity/pedido/Pedido.java
+++ src/main/java/com/acert/delivery/entity/pedido/Pedido.java
@@ -1,15 +1,20 @@
 package com.acert.delivery.entity.pedido;
 
+import java.io.Serializable;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.acert.delivery.entity.entrega.Entrega;
+import com.acert.delivery.entity.produto.Produto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,7 +29,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
-public class Pedido {
+public class Pedido implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,4 +38,7 @@ public class Pedido {
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "entrega_id", referencedColumnName = "id")
 	private Entrega entrega;
+
+	@OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Produto> produtos;
 }
